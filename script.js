fd24729ebbc0be4c300125204b1a4fe6080dc834
KARTHIKEYAN R <karthikeyanr2703@gmail.com>
2024-02-17 16:46:45 +0530

    files on work

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ let left = document.querySelector('#left');
 let right = document.querySelector('#right');
 let frame= document.querySelector("#frame");
 let images = document.querySelectorAll(".image");
+let bottom = document.querySelector('#bottom');
 const length = images.length;
 let sliderNumber = 1;
 let getFirstSlider = () => {
@@ -29,3 +30,27 @@ right.addEventListener("click",()=>{
 left.addEventListener("click",()=>{
     sliderNumber > 1 ? prevSlide():getLastSlide();
 });
+for(let i=1;i<=length;i++){
+    let div = document.createElement("div");
+    div.className = "button";
+    bottom.appendChild(div);
+
+}
+let buttons = document.querySelectorAll(".button");
+console.log(buttons);
+// buttons[0].style.backgroundColor ="white";
+let resetBg = ()=>{
+    buttons.forEach((button)=>{
+        button.style.backgroundColor ='transparent';
+    
+    })
+}
+buttons.forEach((button,index)=>{
+    resetBg();
+    button.style.backgroundColor ="white";
+    sliderNumber = index + 1;
+    buttons[index].style.transform = ""
+
+
+
+})
